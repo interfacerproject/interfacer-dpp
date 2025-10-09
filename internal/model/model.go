1cb4eba85a150f82fa96a82b02fdda8caf24fe34
phoebus-84 <ennio@dyne.org>
2025-10-09 21:35:14 +0200

    fix: mongodb passports colection data model

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,11 +1,17 @@
 package model
 
 import (
-	"github.com/oklog/ulid/v2" 
+	"github.com/oklog/ulid/v2"
 )
 
+type TransformedValue struct {
+	Type  string      `json:"type" bson:"type"`
+	Value any `json:"value" bson:"value"`
+	Units string      `json:"units,omitempty" bson:"units,omitempty"`
+}
+
 type DigitalProductPassport struct {
-	ID                       ulid.ULID               `json:"_id,omitempty" bson:"_id,omitempty"`
+	ID                       ulid.ULID                `json:"id" bson:"_id"`
 	ProductOverview          ProductOverview          `json:"productOverview" bson:"productOverview"`
 	Reparability             Reparability             `json:"reparability" bson:"reparability"`
 	EnvironmentalImpact      EnvironmentalImpact      `json:"environmentalImpact" bson:"environmentalImpact"`
@@ -21,97 +27,97 @@ type DigitalProductPassport struct {
 }
 
 type ProductOverview struct {
-	BrandName                     string `json:"brandName" bson:"brandName"`
-	ProductImage                  string `json:"productImage" bson:"productImage"`
-	GlobalProductClassificationCode string `json:"globalProductClassificationCode" bson:"globalProductClassificationCode"`
-	CountryOfSale                 string `json:"countryOfSale" bson:"countryOfSale"`
-	ProductDescription            string `json:"productDescription" bson:"productDescription"`
-	ProductName                   string `json:"productName" bson:"productName"`
-	NetWeight                     string `json:"netWeight" bson:"netWeight"`
-	Gtin                          string `json:"gtin" bson:"gtin"`
-	Color                         string `json:"color" bson:"color"`
-	CountryOfOrigin               string `json:"countryOfOrigin" bson:"countryOfOrigin"`
-	Dimensions                    string `json:"dimensions" bson:"dimensions"`
-	ModelName                     string `json:"modelName" bson:"modelName"`
-	TaricCode                     string `json:"taricCode" bson:"taricCode"`
-	ConditionOfTheProduct         string `json:"conditionOfTheProduct" bson:"conditionOfTheProduct"`
-	NetContent                    string `json:"netContent" bson:"netContent"`
-	NominalMaximumRPM             string `json:"nominalMaximumRPM" bson:"nominalMaximumRPM"`
-	MaximumDrillingDiameter       string `json:"maximumDrillingDiameter" bson:"maximumDrillingDiameter"`
-	NumberOfGears                 string `json:"numberOfGears" bson:"numberOfGears"`
-	Torque                        string `json:"torque" bson:"torque"`
-	WarrantyDuration              string `json:"warrantyDuration" bson:"warrantyDuration"`
-	SafetyInstructions            string `json:"safetyInstructions" bson:"safetyInstructions"`
+	BrandName                     TransformedValue `json:"brandName" bson:"brandName"`
+	ProductImage                  TransformedValue `json:"productImage" bson:"productImage"`
+	GlobalProductClassificationCode TransformedValue `json:"globalProductClassificationCode" bson:"globalProductClassificationCode"`
+	CountryOfSale                 TransformedValue `json:"countryOfSale" bson:"countryOfSale"`
+	ProductDescription            TransformedValue `json:"productDescription" bson:"productDescription"`
+	ProductName                   TransformedValue `json:"productName" bson:"productName"`
+	NetWeight                     TransformedValue `json:"netWeight" bson:"netWeight"`
+	Gtin                          TransformedValue `json:"gtin" bson:"gtin"`
+	Color                         TransformedValue `json:"color" bson:"color"`
+	CountryOfOrigin               TransformedValue `json:"countryOfOrigin" bson:"countryOfOrigin"`
+	Dimensions                    TransformedValue `json:"dimensions" bson:"dimensions"`
+	ModelName                     TransformedValue `json:"modelName" bson:"modelName"`
+	TaricCode                     TransformedValue `json:"taricCode" bson:"taricCode"`
+	ConditionOfTheProduct         TransformedValue `json:"conditionOfTheProduct" bson:"conditionOfTheProduct"`
+	NetContent                    TransformedValue `json:"netContent" bson:"netContent"`
+	NominalMaximumRPM             TransformedValue `json:"nominalMaximumRPM" bson:"nominalMaximumRPM"`
+	MaximumDrillingDiameter       TransformedValue `json:"maximumDrillingDiameter" bson:"maximumDrillingDiameter"`
+	NumberOfGears                 TransformedValue `json:"numberOfGears" bson:"numberOfGears"`
+	Torque                        TransformedValue `json:"torque" bson:"torque"`
+	WarrantyDuration              TransformedValue `json:"warrantyDuration" bson:"warrantyDuration"`
+	SafetyInstructions            TransformedValue `json:"safetyInstructions" bson:"safetyInstructions"`
 }
 
 type Reparability struct {
-	ServiceAndRepairInstructions string `json:"serviceAndRepairInstructions" bson:"serviceAndRepairInstructions"`
-	AvailabilityOfSpareParts     string `json:"availabilityOfSpareParts" bson:"availabilityOfSpareParts"`
+	ServiceAndRepairInstructions TransformedValue `json:"serviceAndRepairInstructions" bson:"serviceAndRepairInstructions"`
+	AvailabilityOfSpareParts     TransformedValue `json:"availabilityOfSpareParts" bson:"availabilityOfSpareParts"`
 }
 
 type EnvironmentalImpact struct {
-	WaterConsumptionPerUnit  string `json:"waterConsumptionPerUnit" bson:"waterConsumptionPerUnit"`
-	ChemicalConsumptionPerUnit string `json:"chemicalConsumptionPerUnit" bson:"chemicalConsumptionPerUnit"`
-	Co2eEmissionsPerUnit     string `json:"co2eEmissionsPerUnit" bson:"co2eEmissionsPerUnit"`
-	EnergyConsumptionPerUnit string `json:"energyConsumptionPerUnit" bson:"energyConsumptionPerUnit"`
+	WaterConsumptionPerUnit  TransformedValue `json:"waterConsumptionPerUnit" bson:"waterConsumptionPerUnit"`
+	ChemicalConsumptionPerUnit TransformedValue `json:"chemicalConsumptionPerUnit" bson:"chemicalConsumptionPerUnit"`
+	Co2eEmissionsPerUnit     TransformedValue `json:"co2eEmissionsPerUnit" bson:"co2eEmissionsPerUnit"`
+	EnergyConsumptionPerUnit TransformedValue `json:"energyConsumptionPerUnit" bson:"energyConsumptionPerUnit"`
 }
 
 type ComplianceAndStandards struct {
-	CeMarking      string `json:"ceMarking" bson:"ceMarking"`
-	RohsCompliance string `json:"rohsCompliance" bson:"rohsCompliance"`
+	CeMarking      TransformedValue `json:"ceMarking" bson:"ceMarking"`
+	RohsCompliance TransformedValue `json:"rohsCompliance" bson:"rohsCompliance"`
 }
 
 type Certificates struct {
-	NameOfCertificate string `json:"nameOfCertificate" bson:"nameOfCertificate"`
+	NameOfCertificate TransformedValue `json:"nameOfCertificate" bson:"nameOfCertificate"`
 }
 
 type Recyclability struct {
-	RecyclingInstructions string `json:"recyclingInstructions" bson:"recyclingInstructions"`
-	MaterialComposition   string `json:"materialComposition" bson:"materialComposition"`
-	SubstancesOfConcern   string `json:"substancesOfConcern" bson:"substancesOfConcern"`
+	RecyclingInstructions TransformedValue `json:"recyclingInstructions" bson:"recyclingInstructions"`
+	MaterialComposition   TransformedValue `json:"materialComposition" bson:"materialComposition"`
+	SubstancesOfConcern   TransformedValue `json:"substancesOfConcern" bson:"substancesOfConcern"`
 }
 
 type EnergyUseAndEfficiency struct {
-	BatteryType           string `json:"batteryType" bson:"batteryType"`
-	BatteryChargingTime   string `json:"batteryChargingTime" bson:"batteryChargingTime"`
-	BatteryLife           string `json:"batteryLife" bson:"batteryLife"`
-	ChargerType           string `json:"chargerType" bson:"chargerType"`
-	MaximumElectricalPower string `json:"maximumElectricalPower" bson:"maximumElectricalPower"`
-	MaximumVoltage        string `json:"maximumVoltage" bson:"maximumVoltage"`
-	MaximumCurrent        string `json:"maximumCurrent" bson:"maximumCurrent"`
-	PowerRating           string `json:"powerRating" bson:"powerRating"`
-	DcVoltage             string `json:"dcVoltage" bson:"dcVoltage"`
+	BatteryType           TransformedValue `json:"batteryType" bson:"batteryType"`
+	BatteryChargingTime   TransformedValue `json:"batteryChargingTime" bson:"batteryChargingTime"`
+	BatteryLife           TransformedValue `json:"batteryLife" bson:"batteryLife"`
+	ChargerType           TransformedValue `json:"chargerType" bson:"chargerType"`
+	MaximumElectricalPower TransformedValue `json:"maximumElectricalPower" bson:"maximumElectricalPower"`
+	MaximumVoltage        TransformedValue `json:"maximumVoltage" bson:"maximumVoltage"`
+	MaximumCurrent        TransformedValue `json:"maximumCurrent" bson:"maximumCurrent"`
+	PowerRating           TransformedValue `json:"powerRating" bson:"powerRating"`
+	DcVoltage             TransformedValue `json:"dcVoltage" bson:"dcVoltage"`
 }
 
 type ComponentInformation struct {
-	ComponentDescription string `json:"componentDescription" bson:"componentDescription"`
-	ComponentGTIN        string `json:"componentGTIN" bson:"componentGTIN"`
-	LinkToDPP            string `json:"linkToDPP" bson:"linkToDPP"`
+	ComponentDescription TransformedValue `json:"componentDescription" bson:"componentDescription"`
+	ComponentGTIN        TransformedValue `json:"componentGTIN" bson:"componentGTIN"`
+	LinkToDPP            TransformedValue `json:"linkToDPP" bson:"linkToDPP"`
 }
 
 type EconomicOperator struct {
-	CompanyName      string `json:"companyName" bson:"companyName"`
-	Gln              string `json:"gln" bson:"gln"`
-	EoriNumber       string `json:"eoriNumber" bson:"eoriNumber"`
-	AddressLine1     string `json:"addressLine1" bson:"addressLine1"`
-	AddressLine2     string `json:"addressLine2" bson:"addressLine2"`
-	ContactInformation string `json:"contactInformation" bson:"contactInformation"`
+	CompanyName      TransformedValue `json:"companyName" bson:"companyName"`
+	Gln              TransformedValue `json:"gln" bson:"gln"`
+	EoriNumber       TransformedValue `json:"eoriNumber" bson:"eoriNumber"`
+	AddressLine1     TransformedValue `json:"addressLine1" bson:"addressLine1"`
+	AddressLine2     TransformedValue `json:"addressLine2" bson:"addressLine2"`
+	ContactInformation TransformedValue `json:"contactInformation" bson:"contactInformation"`
 }
 
 type RepairInformation struct {
-	ReasonForRepair string `json:"reasonForRepair" bson:"reasonForRepair"`
-	PerformedAction string `json:"performedAction" bson:"performedAction"`
-	MaterialsUsed   string `json:"materialsUsed" bson:"materialsUsed"`
-	DateOfRepair    string `json:"dateOfRepair" bson:"dateOfRepair"`
+	ReasonForRepair TransformedValue `json:"reasonForRepair" bson:"reasonForRepair"`
+	PerformedAction TransformedValue `json:"performedAction" bson:"performedAction"`
+	MaterialsUsed   TransformedValue `json:"materialsUsed" bson:"materialsUsed"`
+	DateOfRepair    TransformedValue `json:"dateOfRepair" bson:"dateOfRepair"`
 }
 
 type RefurbishmentInformation struct {
-	PerformedAction     string `json:"performedAction" bson:"performedAction"`
-	MaterialsUsed       string `json:"materialsUsed" bson:"materialsUsed"`
-	DateOfRefurbishment string `json:"dateOfRefurbishment" bson:"dateOfRefurbishment"`
+	PerformedAction     TransformedValue `json:"performedAction" bson:"performedAction"`
+	MaterialsUsed       TransformedValue `json:"materialsUsed" bson:"materialsUsed"`
+	DateOfRefurbishment TransformedValue `json:"dateOfRefurbishment" bson:"dateOfRefurbishment"`
 }
 
 type RecyclingInformation struct {
-	PerformedAction string `json:"performedAction" bson:"performedAction"`
-	DateOfRecycling string `json:"dateOfRecycling" bson:"dateOfRecycling"`
+	PerformedAction TransformedValue `json:"performedAction" bson:"performedAction"`
+	DateOfRecycling TransformedValue `json:"dateOfRecycling" bson:"dateOfRecycling"`
 }
